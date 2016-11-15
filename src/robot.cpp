d0a5dd7afd5ba49fc99df80ba503066e22b2f3af
Antoine Rabenandrasana <antoine.rabenandrasana@etu.unice.fr>
2016-11-15 19:16:54 +0100

    Fonction dans Robot.cpp

--- src/robot.cpp
+++ src/robot.cpp
@@ -1,10 +1,37 @@
 #include "robot.h"
 #include <string>
+#include <iostream>
 using namespace std;
+
 Robot::Robot(string direction,Position pos,Plot* ptr_plot,EtatRobot* mon_etat,Objet* ptr_obj){
 	this->direction=direction;
 	this->pos = pos;
 	this->ptr_plot=ptr_plot;
 	this->ptr_obj=ptr_obj;
 	this->mon_etat=mon_etat;
+}
+
+void Robot::saisir(Objet* o){
+	try{
+		(this->mon_etat)->saisir();
+		this->ptr_obj=o;
+	}catch(exception& e){
+		cout<< e.what()<<endl;
+	}
+}
+
+void Robot::figer(){
+	try{
+		(this->mon_etat)->figer();
+	}catch(exception& e){
+		cout<< e.what()<<endl;
+	}
+}
+
+void Robot::repartir(){
+	try{
+		(this->mon_etat)->repartir();
+	}catch(exception& e){
+		cout<< e.what()<<endl;
+	}
 }
